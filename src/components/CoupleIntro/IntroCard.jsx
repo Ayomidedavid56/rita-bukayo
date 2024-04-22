082b27daf0f1ad0f80dd36f3d48b1c6bfeba2a0f
Ayomidedavid56 <ayomidedavid56@yahoo.com>
2024-04-22 21:39:09 +0100

    first commit

--- src/components/CoupleIntro/IntroCard.jsx
+++ src/components/CoupleIntro/IntroCard.jsx
@@ -1,4 +1,4 @@
-import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+// import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 import "./index.css";
 
 // eslint-disable-next-line react/prop-types
@@ -9,8 +9,8 @@ export default function IntroCard({ image, label, value }) {
         <span className="names_titles">THE {label}</span>
         <h2>{value}</h2>
         <div className="about_pic_container">
-          <span className="about_picframe"></span>
-          <div className="about_social">
+          {/* <span className="about_picframe"></span> */}
+          {/* <div className="about_social">
             <a href="#">
               <FaInstagram size={30} style={{ color: "#ffffff" }} />
             </a>
@@ -20,7 +20,7 @@ export default function IntroCard({ image, label, value }) {
             <a href="#">
               <FaTwitter size={30} style={{ color: "#ffffff" }} />
             </a>
-          </div>
+          </div> */}
           <div className="about_pic">
             <img src={image} alt="" title="" />
           </div>
